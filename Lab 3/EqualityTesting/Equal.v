a5f0854f00b16aeaa891a8b752f1a86abf75a44f
kenne24 <kensworthwilliams@gmail.com>
2018-09-22 00:26:19 -0600

    Update Equal.v

--- Lab 3/EqualityTesting/Equal.v
+++ Lab 3/EqualityTesting/Equal.v
@@ -18,24 +18,19 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module Equal(input [7:0] num1, num2, output o);
+module equal(input[7:0]num1, num2, output o);
+	wire bit0, bit1, bit2, bit3, bit4, bit5, bit6, bit7;
+	//checks if the bits to compare are equal
+	equal equal1(.o(bit0),.num1(num1[0]),.num2(num2[0]));
+	equal equal2(.o(bit1),.num1(num1[1]),.num2(num2[1]));
+	equal equal3(.o(bit2),.num1(num1[2]),.num2(num2[2]));
+	equal equal4(.o(bit3),.num1(num1[3]),.num2(num2[3]));
+	equal equal5(.o(bit4),.num1(num1[4]),.num2(num2[4]));
+	equal equal6(.o(bit5),.num1(num1[5]),.num2(num2[5]));
+	equal equal7(.o(bit6),.num1(num1[6]),.num2(num2[6]));
+	equal equal8(.o(bit7),.num1(num1[7]),.num2(num2[7]));
 
-	reg[7:0] temp;
-	reg result;
-	
-	// XOR pairs (gives 0 once two inputs are the same)
-	assign temp[0] = num1[0] ^ num2[0];
-	assign temp[1] = num1[1] ^ num2[1];
-	assign temp[2] = num1[2] ^ num2[2];
-	assign temp[3] = num1[3] ^ num2[3];
-	assign temp[4] = num1[4] ^ num2[4];
-	assign temp[5] = num1[5] ^ num2[5];
-	assign temp[6] = num1[6] ^ num2[6];
-	assign temp[7] = num1[7] ^ num2[7];
-	
-	//implements a NOR gate to give result required
-	//result will be a 1 if all inputs are 0 (which from the XOR gate above means all inputs are the same
-	//result will be a 0 if any of the inputs is a 1 (meaning at least one XOR comparison was different
-	assign result = ~(temp[0] | temp[1] | temp[2] | temp[3] | temp[4] | temp[5] | temp[6] | temp[7]);
+	//ANDs the results of equality and assign output to 1 or 0 accordingly
+	assign o = bit0 & bit1 & bit2 & bit3 & bit4 & bit5 & bit6 & bits7;
 
 endmodule
